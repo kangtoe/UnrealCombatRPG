e20c2be021cc9bb17d453aea44b9bc517ccd1346
kangtoe <kangtoe99@gmail.com>
2024-01-11 12:59:32 +0900

    프로젝트 생성

---
+++ Source/UnrealStart02/UnrealStart02GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealStart02GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALSTART02_API AUnrealStart02GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
